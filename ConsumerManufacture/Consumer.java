e07b6f194181547b94670e8a716db8d26e269c0f
proninaps <114224294+proninaps@users.noreply.github.com>
2022-12-22 22:19:05 +0300

    Update Consumer.java

--- ConsumerManufacture/Consumer.java
+++ ConsumerManufacture/Consumer.java
@@ -1,27 +1,27 @@
 import java.util.Queue;
 
-public class Consumer implements Runnable {
-    private final Queue<Integer> buffer;
-    private final int size;
-    public Consumer(Queue<Integer> buffer, int size){
+public class Consumer implements Runnable { // создаем класс потребитея наследуемый от Runnable
+    private final Queue<Integer> buffer;// создаю общую очередь для потоков
+    private final int size;// максимальную вместимость буффера
+    public Consumer(Queue<Integer> buffer, int size){// конструктор
         this.buffer = buffer;
         this.size = size;
     }
-    private Integer GetAll() throws InterruptedException {
+    private Integer GetAll() throws InterruptedException {// метод потребления 
         synchronized (buffer){
-            if (buffer.size()==0){
+            if (buffer.size()==0){// если буфер пустой то поток ждет когда он заполнится
                 buffer.wait();
             }
-            buffer.notifyAll();
-            return buffer.poll();
+            buffer.notifyAll();//уведомляем второй поток 
+            return buffer.poll();//удаляем число из начала очереди и возвращаем 
 
         }
     }
-    public void run() {
+    public void run() {//расширяем run 
         while (true) {
             try {
                 System.out.println("Потребитель потребил буфер: " + GetAll());
-            } catch (InterruptedException e) {
+            } catch (InterruptedException e) {// ловим исключения 
                 e.printStackTrace();
             }
         }
