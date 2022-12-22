7b50bc9a7c2e5667e4ea6755f3818c41d1e40b3f
proninaps <114224294+proninaps@users.noreply.github.com>
2022-12-22 21:50:18 +0300

    Update Main.java

--- Is_it_in_Mas/Main.java
+++ Is_it_in_Mas/Main.java
@@ -3,18 +3,18 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         int n = 0;
-        int[] array = FullArray();
-        int first_el = array[0];
-        int last_el = array[array.length-1];
-        long time_enumeration = System.currentTimeMillis();
-        if (Check_by_enumeration(array, n)==true){
+        int[] array = FullArray(); // заполняем массив 
+        int first_el = array[0];// ищем первый элемент для бинарного 
+        int last_el = array[array.length-1];// последний эл
+        long time_enumeration = System.currentTimeMillis();// начальный счетчик времни
+        if (Check_by_enumeration(array, n)==true){// по значению проверки выводим сообщение 
             System.out.println("Элемент входит в массив");
         }
         else {
             System.out.println("Элемент не входит в массив");
         }
         System.out.println("Время перебора" + " " + (System.currentTimeMillis() - time_enumeration));
-        long time_binary = System.currentTimeMillis();
+        long time_binary = System.currentTimeMillis();// аналогично с бинарным
         if (Check_binary(array, first_el, last_el, n)==true){
             System.out.println("Элемент входит в массив");
         }
@@ -23,39 +23,39 @@ public class Main {
         }
         System.out.println("Время бинарного" + " " + (System.currentTimeMillis() - time_binary));
     }
-    public static int[] FullArray(){
+    public static int[] FullArray(){ // метод заполнения массива 
         int[] array;
-        array = new int[100000000];
+        array = new int[100000000];// размер 100000000
         for (int i = 0; i < array.length; i++) {
-            array[i] = ((int)(Math.random() * array.length+1 ));
+            array[i] = ((int)(Math.random() * array.length+1 ));// генерируем рандомные числа в том же диапозоне, что и размер массива
         }
-        Arrays.sort(array);
+        Arrays.sort(array);// сортируем массив для бинарного поиска
         return array;
     }
-    public static boolean Check_by_enumeration(int[] array, int n){
+    public static boolean Check_by_enumeration(int[] array, int n){//поиск перебором
         boolean check = false;
-        for (int i=0; i<array.length; i++) {
+        for (int i=0; i<array.length; i++) {//проходимся по всему массиву ищем эллемент массива совпадающий с числом n
             if (array[i] == n) {
-                check = true;
+                check = true; // если находим возвращаем правду если нет оставляем ложь
             }
         }
         return check;
     }
-    public static boolean Check_binary(int [] array,int first_el, int last_el, int n) {
+    public static boolean Check_binary(int [] array,int first_el, int last_el, int n) {// бинарный поиск 
         boolean check = false;
-        if (first_el<=last_el){
-            int mid = first_el + (last_el - first_el) / 2; //=5 =2 =1
-            if (array[mid] == n) {
+        if (first_el<=last_el){// проверяем больше ли первый эллемент второго 
+            int mid = first_el + (last_el - first_el) / 2; //ищем середину отрезка 
+            if (array[mid] == n) {//если середина отрезка или оставшийся элемент совпадает с n выводим правду 
                 check = true;
-            } else if (array[mid] > n) {
+            } else if (array[mid] > n) {// если середина больше то снова проходим бинарный поиск но в первой половине отрезка
                 return Check_binary(array, first_el, mid - 1, n);
-            } else if (array[mid] < n) {
+            } else if (array[mid] < n) {// меньше - во второй 
                 return Check_binary(array, mid + 1, last_el, n);
             }
         else{
             check = false;
             }
         }
-        return check;
+        return check;// возвращаем значение проверки
     }
 }
