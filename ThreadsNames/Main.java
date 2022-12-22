1677991b095b1311710c3583a1c2f1a1f440e149
proninaps <114224294+proninaps@users.noreply.github.com>
2022-12-22 21:53:18 +0300

    Update Main.java

--- ThreadsNames/Main.java
+++ ThreadsNames/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] strings) {
-        Object lock = new Object();
-        ChangeThread Thread1  = new ChangeThread(lock);
+        Object lock = new Object(); // создаю объект ключа
+        ChangeThread Thread1  = new ChangeThread(lock); // два потока
         ChangeThread Thread2  = new ChangeThread(lock);
-        Thread1.start();
+        Thread1.start();// запускаю оба потока
         Thread2.start();
 
     }
