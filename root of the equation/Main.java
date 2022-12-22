77df1f616b6b8e613a449c766509f4c8afd9f3a8
proninaps <114224294+proninaps@users.noreply.github.com>
2022-12-22 20:59:37 +0300

    Update Main.java
    добавлены комментарии

--- root of the equation/Main.java
+++ root of the equation/Main.java
@@ -7,18 +7,18 @@ public class Main {
         System.out.println(check_the_root(1, 10));
     }
     public static double solve_the_equation(double x) {
-        return Math.cos(Math.pow(x, 5)) + Math.pow(x, 4) - 345.3 * x - 23;
+        return Math.cos(Math.pow(x, 5)) + Math.pow(x, 4) - 345.3 * x - 23;// метод производящий вычисления уравнения
     }
     public static double check_the_root(double seg_start, double seg_end){
-        if (seg_end-seg_start <= 0.001){
-            return seg_start;
+        if (seg_end-seg_start <= 0.001){ // проверяем на точность 0,001
+            return seg_start;// возвращаем корень уравнения
         }
-        double mid = seg_start + (seg_end - seg_start)/2;
-        if (solve_the_equation(seg_start)<0 && solve_the_equation(mid)>0 || solve_the_equation(seg_start)>0 && solve_the_equation(mid)<0){
-            return check_the_root(seg_start, mid);
+        double mid = seg_start + (seg_end - seg_start)/2;// ищем середину заданного отрезка
+        if (solve_the_equation(seg_start)<0 && solve_the_equation(mid)>0 || solve_the_equation(seg_start)>0 && solve_the_equation(mid)<0){//проверяем возможные корни на знаки 
+            return check_the_root(seg_start, mid); // снова заходим в метод и проверям подходящий отрезок 
         }
         else{
-            return check_the_root(mid, seg_end);
+            return check_the_root(mid, seg_end); // снова заходим в метод и проверям подходящий отрезок 
         }
     }
 
