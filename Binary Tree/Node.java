bedae7ecb9e87895f7f72e981d8f604d55fda6f4
proninaps <114224294+proninaps@users.noreply.github.com>
2022-12-22 21:26:54 +0300

    Update Node.java

--- Binary Tree/Node.java
+++ Binary Tree/Node.java
@@ -1,23 +1,23 @@
-public class Node {
+public class Node { // класс узлов
     int value;
-    Node rightChild;
+    Node rightChild; 
     Node leftChild;
 
 
 
-    public void set_Value(int value){
+    public void set_Value(int value){ // меняет значение ключа узла
         this.value = value;
     }
-    public int get_Value(){
+    public int get_Value(){ // выводит значение ключа узла
         return this.value;
     }
-    public void set_RightChild(Node rightChild){
+    public void set_RightChild(Node rightChild){// аналогично с правм(большим) узлом наследником
         this.rightChild = rightChild;
     }
     public Node get_RightChild(){
         return this.rightChild;
     }
-    public void set_LeftChild(Node leftChild){
+    public void set_LeftChild(Node leftChild){// аналогично с левым(меньшим) узлом наследником
         this.leftChild = leftChild;
     }
     public Node get_LeftChild(){
